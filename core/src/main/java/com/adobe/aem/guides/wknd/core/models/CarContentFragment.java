894aeb14e51828d1d71aa6972c2a814be59b0911
ranjanayadav56 <ranjanayadav@rightpoint.com>
2023-05-26 13:08:35 +0530

    Nested Car Content Fragment

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/CarContentFragment.java
@@ -0,0 +1,11 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+public interface CarContentFragment {
+
+      String getBrandName();
+      String getIndustryName();
+      String getColor();
+      String getCarImage();
+      boolean isEmpty();
+      
+}
